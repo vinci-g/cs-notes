948e58bac287871ec74f2a32d8defe81391be20c
vinci_g <10341898-vinci_g@users.noreply.gitlab.com>
2024-03-07 19:09:11 +0800

    add ch7 sample programs

---
+++ C/sample-program/length2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+    int len = 0;
+
+    printf("Enter a message: ");
+    while (getchar() != '\n') {
+        len ++;
+    }
+    printf("Your message was %d character(s) long.\n", len);
+    return 0;
+}
