16ad5009354a621698817c05edc72c2759446cfe
vinci_g <10341898-vinci_g@users.noreply.gitlab.com>
2024-03-13 13:55:10 +0800

    add ch8 projects

---
+++ C/projects/ch8/8.3.c
@@ -0,0 +1,41 @@
+// checks numbers for repeated digits
+
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(void) {
+    int digit_seen[10] = {0}; 
+    int digit;
+    long n;
+
+    printf("Enter a number: ");
+    scanf("%ld", &n);
+
+    while (n > 0) {
+      while (n > 0) {
+        digit = n % 10;
+        digit_seen[digit] += 1;
+        n /= 10;
+      }
+
+      printf("Digit:\t\t");
+      for (int i = 0; i < 10; i++) {
+        printf(" %d", i);
+      }
+      printf("\n");
+
+      printf("Occurences:\t");
+      for (int i = 0; i < 10; i++) {
+        printf(" %d", digit_seen[i]);
+      }
+      printf("\n");
+
+      for (int i = 0; i < 10; i++) {
+          digit_seen[i] = 0;
+      }
+      printf("Enter a number: ");
+      scanf("%ld", &n);
+    }
+    return 0;
+}
+
