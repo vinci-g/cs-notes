431781f089b0da0458537ac5d2e64a74fa268dc7
vinci_g <10341898-vinci_g@users.noreply.gitlab.com>
2024-03-13 21:16:54 +0800

    add ch8 projects

--- C/projects/ch8/8.8.c
+++ C/projects/ch8/8.8.c
@@ -1,33 +1,30 @@
 #include <stdio.h>
 
 int main(void) {
-    int arr[5][5], rsum[5] = {0}, csum[5] = {0}, n;
+    int arr[5][5], rsum[5] = {0}, asum[5] = {0}, n;
 
     for (int i = 0; i < 5; i++) {
-        printf("Enter row %d: ", i + 1);
+        printf("Student %d grades: ", i + 1);
         for (int j = 0; j < 5; j++) {
             scanf(" %d", &n);
             arr[i][j] = n;
         }
     }
 
-    printf("Row totals: ");
     for (int i = 0; i < 5; i++) {
         for (int j = 0; j < 5; j++) {
             rsum[i] += arr[i][j];
         }
-        printf(" %d", rsum[i]);
+        printf("Student %d Total Grade: %d\n", i + 1, rsum[i]);
     }
-    printf("\n");
 
-    printf("Column totals: ");
     for (int i = 0; i < 5; i++) {
         for (int j = 0; j < 5; j++) {
-            csum[i] += arr[j][i];
+            asum[i] += arr[i][j];
         }
-        printf(" %d", csum[i]);
+        double ave = (double) rsum[i]/5;
+        printf("Student %d Average Grade: %.2lf\n", i + 1, ave);
     }
-    printf("\n");
 
     return 0;
 }
